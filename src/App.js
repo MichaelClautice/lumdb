7c5f58c6e17e2a2db0e45114c1373fae59c0b135
Michael Clautice <michaelclauticeusa@gmail.com>
2018-09-17 09:55:54 -0400

    Mon 9.17.18

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 /* eslint react/no-did-mount-set-state: 0 */
+// importing what is needed...
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Movie from "./Movie.js";
 
-
+// test comment Mon Sept 17, 2018 Hello There!
+// this class will instantiate the App parent component...
 class App extends Component {
-
+// assigning the state of this component
   state = {
     movies: []
   }
-
+// connecting w the movie db
   async componentDidMount() {
     try {
       const result = await fetch("https://api.themoviedb.org/3/discover/movie?api_key=390a0dae875dea79ef1ba7b008eb0313&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1");
@@ -27,12 +33,14 @@ class App extends Component {
   render() {
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} /> )}
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} /> )}
+        </div>
+      </Router>
     );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154'
+
 const Movie = ({ movie }) => (
     <div>
-        <h3>{movie.title}</h3>
+        <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
     </div>
     );
 
-export default Movie;
+// const Movie = ({ movie }) => (
+//     <div>
+//             <h3>{movie.title}</h3>
+//     </div>
+//     );
 
+export default Movie;
+// test comment2
 
 Movie.propTypes = {
     movie: PropTypes.shape({
